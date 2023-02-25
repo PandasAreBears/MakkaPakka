773328b90e37350d1a69a039e9d601154d09697a
Alex J <blah@blah.com>
2023-02-25 16:11:42 +0000

    added examples files with make script

---
+++ examples/make.sh
@@ -0,0 +1,11 @@
+for file in *; do
+    if [ "mkpk" = ${file##*.} ]; then
+        ASM_PATH=/tmp/${file%.*}.asm
+        OBJ_PATH=/tmp/${file%.*}.o
+        EXE_PATH=/tmp/${file%.*}
+
+        python3 ../makka_pakka/mkpk_transpile.py ${file} -o ${ASM_PATH}
+        nasm -f elf64 -o ${OBJ_PATH} ${ASM_PATH}
+        ld ${OBJ_PATH} -o ${EXE_PATH}
+    fi
+done
