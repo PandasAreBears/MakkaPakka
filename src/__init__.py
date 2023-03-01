9adad428c53b15d190c1fe594519673164f7368d
Alex J <blah@blah.com>
2023-03-01 18:10:18 +0000

    remove extraneous init file

--- src/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-A programming language which provides addtional functionality onto of
-traditional assembly (such as functions). The language features ROP injection
-replacement automatically, using pre-defined call addresses. The language
-translates down to .asm - a convienience for using elf_caver.
-"""
